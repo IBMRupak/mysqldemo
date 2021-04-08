4cb5f5deb95d7c79156ef985f7183297c37139ed
unknown <ibmjavanh20@iiht.tech>
2021-04-08 16:59:48 +0530

    Added validation and used ajax

---
+++ src/main/java/com/example/mysqldemo/entity/User.java
@@ -0,0 +1,48 @@
+package com.example.mysqldemo.entity;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@NotNull
+	@NotBlank
+	private String name;
+	@Email(message="Valid Email")
+	private String email;
+	private long mobilenumber;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getMobile() {
+		return mobilenumber;
+	}
+	public void setMobile(long mobilenumber) {
+		this.mobilenumber = mobilenumber;
+	}
+
+}
